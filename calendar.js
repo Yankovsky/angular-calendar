01d16b552ce32473dd5005e07ad573fa5240ab2c
Yankovsky Andrey <YankovskyAndrey@gmail.com>
2015-04-04 14:57:25 +0700

    some styles; add readonly mode; add new scope

--- calendar.js
+++ calendar.js
@@ -1,39 +1,40 @@
-angular.module('calendar', []).directive('calendar', ['$timeout', function($timeout) {
+angular.module("calendar", []).directive("calendar", ["$timeout", function($timeout) {
     // Key event mapper
     var keys = {
-        13: 'enter',
-        32: 'space',
-        33: 'pageup',
-        34: 'pagedown',
-        35: 'end',
-        36: 'home',
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        13: "enter",
+        32: "space",
+        33: "pageup",
+        34: "pagedown",
+        35: "end",
+        36: "home",
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
     }
 
     return {
-        restrict: 'EA',
-        templateUrl: 'calendar.html',
+        restrict: "EA",
+        templateUrl: "calendar.html",
+        scope: true,
         link: function(scope, element, attrs) {
             scope.dayTemplate = attrs.dayTemplate
-            var customClasses = scope.$eval(attrs.customClasses)
-            var onDayClick = scope.$eval(attrs.onDayClick)
+            var customClasses = scope.$parent.$eval(attrs.customClasses)
+            var onDayClick = scope.$parent.$eval(attrs.onDayClick)
 
             var currentDate = moment()
             var minDate = currentDate.clone()
-            var maxDate = currentDate.clone().add(90, 'days')
+            var maxDate = currentDate.clone().add(90, "days")
             var startingDay = 1
 
             scope.move = function(direction) {
-                currentDate.add(direction, 'months')
+                currentDate.add(direction, "months")
                 refreshView()
             }
 
             scope.getClasses = function(day) {
                 var classes = [customClasses(day)]
-                angular.forEach(['disabled', 'secondary'], function(property) {
+                angular.forEach(["disabled", "secondary"], function(property) {
                     if (day[property]) {
                         classes.push(property)
                     }
@@ -41,54 +42,25 @@ angular.module('calendar', []).directive('calendar', ['$timeout', function($time
                 return classes
             }
 
-            scope.handleDayClick = function(day) {
-                if (!day.disabled) {
-                    if (day.secondary) {
-                        scope.move(day.date.isBefore(currentDate) ? -1 : 1)
+            if (!attrs.readonly) {
+                scope.handleDayClick = function(day) {
+                    if (!day.disabled) {
+                        if (day.secondary) {
+                            scope.move(day.date.isBefore(currentDate) ? -1 : 1)
+                        }
+                        onDayClick(day)
                     }
-                    onDayClick(day)
                 }
-            }
-
-            scope.keydown = function(evt) {
-                var key = keys[evt.which]
-
-                if (!key || evt.shiftKey || evt.altKey) {
-                    return
-                }
-
-                evt.preventDefault()
-                evt.stopPropagation()
-                if (key === 'enter' || key === 'space') {
-                    debugger
-                    if (isDisabled(that.activeDate)) {
-                        return // do nothing
-                    }
-                    scope.select(that.activeDate)
-                    focusElement()
-                } else if (evt.ctrlKey && (key === 'up' || key === 'down')) {
-                    scope.toggleMode(key === 'up' ? 1 : -1)
-                    focusElement()
-                } else {
-                    handleKeyDown(key, evt)
-                    refreshView()
-                }
-            }
-
-            function focusElement() {
-                $timeout(function() {
-                    element[0].focus()
-                }, 0, false)
             }
 
             function refreshView() {
-                var firstDayOfMonth = currentDate.clone().startOf('month')
+                var firstDayOfMonth = currentDate.clone().startOf("month")
                 var month = firstDayOfMonth.month()
                 var numDisplayedFromPreviousMonth = (6 + firstDayOfMonth.day() - startingDay) % 7 + 1
-                var startDate = firstDayOfMonth.clone().subtract(numDisplayedFromPreviousMonth, 'days')
+                var startDate = firstDayOfMonth.clone().subtract(numDisplayedFromPreviousMonth, "days")
 
                 scope.moveLeftDisabled = startDate.isBefore(minDate)
-                scope.moveRightDisabled = startDate.clone().add(42, 'days').isAfter(maxDate)
+                scope.moveRightDisabled = startDate.clone().add(42, "days").isAfter(maxDate)
 
                 // 42 is the number of days on a six-row calendar
                 var n = 42
@@ -99,13 +71,13 @@ angular.module('calendar', []).directive('calendar', ['$timeout', function($time
                     var isDisabled = day.isBefore(minDate) || day.isAfter(maxDate)
                     var isSecondary = day.month() !== month
                     days[i++] = {
-                        id: day.format('YYYYMMDD'),
+                        id: day.format("YYYYMMDD"),
                         date: day,
-                        label: day.format('D'),
+                        label: day.format("D"),
                         disabled: isDisabled,
                         secondary: isSecondary
                     }
-                    current.add(1, 'day')
+                    current.add(1, "day")
                 }
 
                 scope.labels = new Array(7)
@@ -113,7 +85,7 @@ angular.module('calendar', []).directive('calendar', ['$timeout', function($time
                     scope.labels[j] = days[j].date.format(attrs.formatDayHeader)
                 }
 
-                scope.title = currentDate.format('MMMM')
+                scope.title = currentDate.format("MMMM")
                 scope.rows = split(days, 7)
             }
 
@@ -123,30 +95,6 @@ angular.module('calendar', []).directive('calendar', ['$timeout', function($time
                     arrays.push(arr.splice(0, size))
                 }
                 return arrays
-            }
-
-            function handleKeyDown(key, evt) {
-                var date = activeDate.getDate()
-
-                if (key === 'left') {
-                    date = date - 1   // up
-                } else if (key === 'up') {
-                    date = date - 7   // down
-                } else if (key === 'right') {
-                    date = date + 1   // down
-                } else if (key === 'down') {
-                    date = date + 7
-                } else if (key === 'pageup' || key === 'pagedown') {
-                    var month = activeDate.getMonth() + (key === 'pageup' ? -1 : 1)
-                    activeDate.setMonth(month, 1)
-                    date = Math.min(getDaysInMonth(activeDate.getFullYear(), activeDate.getMonth()), date)
-                } else if (key === 'home') {
-                    date = 1
-                } else if (key === 'end') {
-                    //document.activeElement
-                    date = getDaysInMonth(activeDate.getFullYear(), activeDate.getMonth())
-                }
-                activeDate.setDate(date)
             }
 
             refreshView()
